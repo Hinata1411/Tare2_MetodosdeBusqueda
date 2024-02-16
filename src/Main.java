f22bdb439848d5cc5bc7fa6a63d637fd38fb1ed9
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-02-16 10:54:58 -0600

    Arreglo de mensajes en consola y orden en el programa

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,22 @@ public class Main {
         //Creamos un arreglo desordenado
         int[] miarreglo = {1, 10, 9, 13, 5, 4, 17, 50, 100};
 
-        //Mostramos el arreglo desordenado
-        System.out.println("Arreglo original: " + Arrays.toString(miarreglo));
-
-        //Primero ordenamos el arreglo
-        Arrays.sort(miarreglo);
-
-        //Mostramos el arreglo ordenado en la consola
-        System.out.println("Arreglo ordenado: " + Arrays.toString(miarreglo));
-
         //Instanciamos el método de Busqueda Binaria
         BusquedaBinaria buscarBin = new BusquedaBinaria();
         //Instanciamos el método de Busqueda Lineal
         BusquedaLineal buscarLin = new BusquedaLineal();
 
+        //Mostramos el arreglo original
+        System.out.println("Arreglo original: " + Arrays.toString(miarreglo));
         //Pedimos al usuario que ingrese el número a buscar y lo guardamos en la variable buscar
-        System.out.println("Ingrese el número a buscar en el arreglo ordenado: ");
+        System.out.println("Ingrese el número a buscar en el arreglo: ");
         Scanner sr = new Scanner(System.in);
         int buscar = sr.nextInt();
 
 
         System.out.println("------BÚSQUEDA LINEAL------");
+        //Mostramos el arreglo original
+        System.out.println("Arreglo original: " + Arrays.toString(miarreglo));
         //Creamos la variable resultadoLin para guardar el resultado obtenido
         //e implementamos el método busquedaLineal colocandole como parámetros
         //el arreglo y la variable buscar
@@ -41,6 +36,10 @@ public class Main {
             System.out.println("El elemento encontrado por búsqueda Lineal esta en el indice: " + resultadoLin);
 
         System.out.println("------BÚSQUEDA BINARIA------");
+        //Primero ordenamos el arreglo
+        Arrays.sort(miarreglo);
+        //Mostramos el arreglo ordenado en la consola
+        System.out.println("Arreglo ordenado: " + Arrays.toString(miarreglo));
         //Creamos la variable resultadoBin para guardar el resultado obtenido
         //e implementamos el método busquedaBinaria colocandole como parámetros
         //el arreglo y la variable buscar que es el dato a buscar
