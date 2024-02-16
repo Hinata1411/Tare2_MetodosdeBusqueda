279a39595a2f5b06e64f6262e50039985ba324e2
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-02-16 09:16:30 -0600

    Creamos el método para busqueda binaria en la clase BusquedaBinaria

--- src/BusquedaBinaria.java
+++ src/BusquedaBinaria.java
@@ -1,6 +1,24 @@
 public class BusquedaBinaria {
 
 
+    //Metodo para implementar la búsqueda binaria
+
+    int busquedaBinaria(Integer miarray[], Integer x){
+
+        int l = 0, r = miarray.length -1;
+        while (l <= r){
+            int m = l + (r - 1)/2;
+
+            if(miarray[m] == x)
+                return m;
+            if(miarray[m] < x)
+                l += m;
+            else
+                r -= m;
+        }
+        return -1;
+    }
+
 
 
 }
